298091e1aeedc4f85cbef2992035e69128311e04
Kévin Bargoin <kevin.bargoin@gmail.com>
2014-04-08 12:49:02 +0200

    Initial commit

---
+++ src/SilexRaven/RavenServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SilexRaven;
+
+/**
+ * Simple Raven Service Provider for Silex PHP micro-framework
+ * @Author  Kévin Bargoin <kevin.bargoin@gmail.com>
+ * @Created 08/04/2014 at 12:45
+ * @Package RavenServiceProvider.php
+ */
+class RavenServiceProvider
+{
+
+} 
